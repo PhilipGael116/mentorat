8e4d853a03bb75b74afce5abc573da5cc55d5b41
Philippe Gael <gaelngando@gmail.com>
2026-03-15 06:08:59 +0100

    added the language translation to local storage

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enTranslation from './locales/en/translation.json';
 import frTranslation from './locales/fr/translation.json';
 
 // Configure i18next
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -16,8 +18,11 @@ i18n
         translation: frTranslation,
       },
     },
-    lng: 'en', // Default language
     fallbackLng: 'en',
+    detection: {
+      order: ['localStorage', 'cookie', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage'],
+    },
     interpolation: {
       escapeValue: false, // React already escapes values to prevent XSS
     },
